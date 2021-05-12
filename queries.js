b72608f46a3ac7d0a7f3d7a1d20c52176a9233d9
Tim McCarthy <thetimmccarthy@gmail.com>
2021-05-11 20:16:21 -0500

    updated connection to postgres

--- queries.js
+++ queries.js
@@ -5,7 +5,9 @@ require('dotenv').config();
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: true
+    ssl: {
+        rejectUnauthorized: false
+      }
   })
 
 const getUsers = async (req, res) => {

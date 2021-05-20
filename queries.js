e0350aa531b3d15cff98d8629d8871b84f31c6d2
Tim McCarthy <thetimmccarthy@gmail.com>
2021-05-20 10:42:41 -0500

    cleaned up code and removed old comments

--- queries.js
+++ queries.js
@@ -1,6 +1,5 @@
 const { Pool, Client } = require('pg');
 const bcrypt = require('bcrypt');
-const { body, validationResult } = require('express-validator');
 require('dotenv').config();
 
 const isProduction = process.env.NODE_ENV == 'production'
